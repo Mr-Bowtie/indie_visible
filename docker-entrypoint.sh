1f3beec74cd71723732a17495922b022425b4e5a
Mr-Bowtie <mr.bowtie989@gmail.com>
2023-08-18 12:40:15 -0400

    feat: add new bin command to refresh container environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,13 +5,21 @@ set -e
 bundle check || bundle install
 
 # server isnt shutting down nicely and managing this file lock, so Im manually doing it for now
-file_path="./tmp/pids/server.pid"  # Replace with the actual path to your file
+file_path="./tmp/pids/server.pid" # Replace with the actual path to your file
 
 if [ -e "$file_path" ]; then
-    rm "$file_path"
-    echo "File deleted: $file_path"
+	rm "$file_path"
+	echo "File deleted: $file_path"
 else
-    echo "No server lock to worry about"
+	echo "No server lock to worry about"
 fi
-bin/rails db:prepare
+
+echo "=========== FRESH = $FRESH ============="
+
+if [ "$FRESH" = "true" ]; then
+	echo "======= Refreshing Container Setup ======="
+	bin/container_setup
+fi
+
+echo "======= Starting Rails server ======="
 bin/rails s -b 0.0.0.0
