6bc14cd6e3d5c994262c4ff60736984302f9cd60
Ian Withers <mr.bowtie989@gmail.com>
2023-10-26 21:31:51 -0400

    chore: clean up javascript entrypoint imports

--- app/frontend/javascript/controllers/index.js
+++ app/frontend/javascript/controllers/index.js
@@ -2,7 +2,5 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { app } from "./app"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
