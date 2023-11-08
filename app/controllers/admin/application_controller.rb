6b9ee76335b021c772a71c37ff3f8cbf76b70cb6
Ian Withers <mr.bowtie989@gmail.com>
2023-11-08 18:01:36 -0500

    feat: add helpers to make associated has_many objects have human readable names
    default is just "class #id" which isnt very helpful when you are trying to select an author or a book
    from a dropdown. no one is going to know what "Book #2" references

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,6 +9,7 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
+    helper_method :decorate_has_many_objects
 
     def authenticate_admin
       # TODO: Add authentication logic here.
@@ -19,5 +20,18 @@ module Admin
     # def records_per_page
     #   params[:per_page] || 20
     # end
+    # INFO: only implemented here because using helper modules did not seem to carry the method thorugh to the views properly
+    private
+
+    def decorate_has_many_objects(resource_arr)
+      resource_arr.map { |resource| [human_readable_id(resource), resource.id] }
+    end
+
+    def human_readable_id(obj)
+      return obj.name if obj.respond_to?(:name)
+      return obj.title if obj.respond_to?(:title)
+
+      "#{obj.class} ##{obj.id}"
+    end
   end
 end
