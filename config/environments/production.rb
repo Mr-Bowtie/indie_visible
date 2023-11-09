4fb2c791258a882ea4183a9f12ee1e44d6b79c47
Ian Withers <mr.bowtie989@gmail.com>
2023-11-08 19:18:37 -0500

    config: setup gmail smtp email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "indie_visible_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'IndieVisible',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # config.action_mailer.default_url_options = { host: ENV.fetch('HOSTNAME') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
