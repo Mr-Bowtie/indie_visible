d7e8b49f028290719a799946ecbd64e553110fd7
Ian Withers <mr.bowtie989@gmail.com>
2023-11-12 14:11:54 -0500

    feat: randomize order of books for gallery

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    collection = Book.all
+    collection = Book.all.order("RANDOM()")
     @filters = {}
 
     # for each param with a real value, apply a filter to the books list
