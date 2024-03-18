aac114ea72fd3dfe9ecc1e6155254eb1515ba061
Ian Withers <mr.bowtie989@gmail.com>
2024-03-18 13:14:37 -0400

    feat: add color_class to tags

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -2,10 +2,11 @@
 #
 # Table name: tags
 #
-#  id         :bigint           not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  color_class :integer
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 FactoryBot.define do
   factory :tag do
